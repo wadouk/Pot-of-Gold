2be765120d87e2c35501de7190cd2e0e84c09d1f
wadouk <devnull@localhost>
2009-09-18 14:42:20 +0000

    ajout de la fenêtre categ

---
+++ src/potofgold/chrome/js/categs.js
@@ -0,0 +1,49 @@
+Components.utils.import("resource://m/debug.js");
+Components.utils.import("resource://m/utils.js");
+Components.utils.import("resource://m/Db.js");
+// var setCellText;
+var treeOther;
+
+var someBuildListener = {
+	willRebuild : function(builder) {
+	},
+	didRebuild : function(builder) {
+
+		var nlAmount = hasAmount(builder.root);
+		if (nlAmount.length != 0)
+			formatCurrency(nlAmount);
+
+	}
+}
+
+function init(event) {
+	try {
+		// dump2(event);
+		var list = getDS(document.firstChild);
+		var ds = getDSLocation();
+		dump2("list.length=" + list.length);
+		// dump2("startEditing");
+		// dump2(treeOther.startEditing);
+		//
+		// dump2("stopEditing");
+		// dump2(treeOther.stopEditing);
+
+		if (event) {
+			dump2("event listener sur tree");
+			treeOther = document.getElementById("tree_categ");
+			//treeOther.addEventListener("DOMAttrModified", editListener, true);
+			treeOther.builder.addListener(someBuildListener);
+		}
+		for (var i = 0; i < list.length; i++) {
+			if (list[i].datasources != ds)
+				list[i].datasources = ds;
+			// list[i].builder
+			list[i].builder.rebuild();
+		}
+
+	} catch (e) {
+		dump2(e);
+	}
+}
+
+window.addEventListener("load", init, true);
